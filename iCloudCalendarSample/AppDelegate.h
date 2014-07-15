9326f2bd8f7cc226cff267263499ce08fff2d983
R. Sharma <rahul2.sharma@Mac132.local>
2014-07-15 18:48:04 +0530

    Initial Commit

---
+++ iCloudCalendarSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iCloudCalendarSample
+//
+//  Created by R. Sharma on 7/15/14.
+//  Copyright (c) 2014 AgileMobileDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iCloudCalendarSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iCloudCalendarSample
+//
+//  Created by R. Sharma on 7/15/14.
+//  Copyright (c) 2014 AgileMobileDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
